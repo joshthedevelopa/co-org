f32a7373a8aa8bd3ece97dc9d9e906f6bb7964ca
Joshua Tetteh <joshua.tetteh@amalitech.com>
2023-01-22 13:45:01 +0000

    Done with basic UI

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,14 @@
+<div class="card">
+    <img src="{{ asset('images/logo.png') }}">
+    <div class="card-content">
+        <h4>Lorem, ipsum dolor sit</h4>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. In, praesentium? Quas animi, aut corporis totam non quos suscipit
+            perferendis ad harum hic nobis rem doloremque quasi ullam? Sapiente, corporis voluptas!
+        </p>
+    </div>
+    <div class="card-actions">
+        <input class="btn primary" type="submit" name="search" value="Chat">
+        <input class="btn primary" type="submit" name="search" value="View">
+    </div>
+</div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,32 @@
 <x-layout title="SF Connect">
-    <div class="filter">
-        
+    <div class="section filter">
+        <span class="heading">Search:</span>
+        <div class="divider"></div>
+        <div class="grid">
+            <div class="form-input">
+                <label>Keyword</label>
+                <input type="text" name="keyword">
+            </div>
+            <div class="form-input">
+                <label>Region</label>
+                <select name="type">
+                    <option value="">-- Select A Option --</option>
+                </select>
+            </div>
+            <div class="form-input">
+                <label>Type</label>
+                <select name="region">
+                    <option value="">-- Select A Option --</option>
+                </select>
+            </div>
+            <div class="form-input">
+                <input class="btn primary" type="submit" name="search" value="Search">
+            </div>
+        </div>
+    </div>
+    <div class="section">
+        <div class="container">
+            <x-card></x-card>
+        </div>
     </div>
 </x-layout>
